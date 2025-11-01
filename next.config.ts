dcf50353aafd05a207ddb7eb0da3de7638661aa6
Priyansh Srivastava <prisriorg@gmail.com>
2025-11-01 23:04:26 +0530

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output:'export',
-  trailingSlash: true, 
+  //  output: "export",
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
 };
-module.exports = nextConfig;
 
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
+export default nextConfig;
